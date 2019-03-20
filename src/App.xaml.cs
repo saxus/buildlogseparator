9bd6715a5cbc084a86a992941039a63cc83aab17
Andor Schindler <schindler.andor@nexogen.hu>
2019-03-20 16:22:56 +0100

    Improve error handling.

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,31 @@ namespace BuildLogSeparator
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var sb = new StringBuilder();
+
+            var ex = e.Exception;
+
+            while (ex != null)
+            {
+                sb.AppendLine($"=== {ex.GetType().Name}");
+                sb.AppendLine(ex.Message);
+                sb.AppendLine();
+                sb.AppendLine(ex.StackTrace); 
+
+                ex = ex.InnerException;
+
+                if (ex != null)
+                {
+                    sb.AppendLine();
+                    sb.AppendLine();
+                }
+            }
+
+            MessageBox.Show(sb.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            e.Handled = true;
+        }
     }
 }
